a276676eb8cbaadb7f2f9ff5201c3dd2d0698733
lmw8864 <lmw8864@gmail.com>
2017-11-22 05:51:54 +0900

    Email setting.

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -142,3 +142,11 @@ CART_SESSION_ID = 'cart_id'
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # default
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "id@gmail.com"
+EMAIL_HOST_PASSWORD = "password"
+EMAIL_USE_TLS = True
