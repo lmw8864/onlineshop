942177324a6d816f4cd7b123d09336dbde6c3943
lmw8864 <lmw8864@gmail.com>
2017-11-22 19:23:52 +0900

    Customized orders.admin.  # Added export_to_csv, order_detail, order_pdf.

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
 from .models import Order, OrderItem
 from cart.cart import Cart
 from .forms import OrderCreateForm
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import weasyprint
 
 
 def order_create(request):
@@ -27,3 +33,19 @@ def order_complete(request):
     order_id = request.GET.get('order_id')
     order = Order.objects.get(id=order_id)
     return render(request, 'orders/order/created.html', {'order': order})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename=order_{}.pdf'.format(order.id)
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATICFILES_DIRS[0]+'/css/pdf.css')])
+    return response
