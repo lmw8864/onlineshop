e7cf8a2983e71af3f2c13e68eaea3e7dd84a2da5
lmw8864 <lmw8864@gmail.com>
2017-11-25 07:07:42 +0900

    Modified templates.  # $ -> ₩ Alter field price on Product and OrderItem.  # decimal_places=0

--- orders/models.py
+++ orders/models.py
@@ -47,7 +47,7 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items')
     product = models.ForeignKey(Product, related_name='order_items')
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=0)
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
